15121d096864f253d3145d963eae000921400466
xdaedx <hewittsunisa@gmail.com>
2021-02-13 12:23:44 -0600

    Added JS

---
+++ index.js
@@ -0,0 +1,265 @@
+//Depancies for it work 
+const inquirer = require('inquirer')
+const { generateMarkdown, writeFile } = require('./utils/generateMarkdown')
+const fs = require('fs');
+
+//Requesting information from user
+inquirer.prompt([
+    {
+        name: `name`,
+        message: `What is your project's name? (Required)`,
+        type: `input`,
+        validate: input =>{
+            if(input){
+                return true
+            } else{
+                console.log(`Please type in a valid name.`)
+                return false
+            }
+        }
+    },
+    {
+        name: `description`,
+        message: `Please input a description to your project:`,
+        type: `input`,
+        validate: input =>{
+            if(input){
+                return true
+            } else{
+                console.log(`Please type in a valid description.`)
+                return false
+            }
+        }
+    },
+    {
+        name: `toc`,
+        message: `Do you want to include Table of Contents?`,
+        type: `confirm`,
+        default: false
+    },
+    {
+        name: `installation`,
+        message: `Please input a description on how to install your project:`,
+        type: `input`,
+        validate: input =>{
+            if(input){
+                return true
+            } else{
+                console.log(`Please type in a valid description.`)
+                return false
+            }
+        }
+    },
+    {
+        name: `usage`,
+        message: `Please input a description on how to use your project:`,
+        type: `input`,
+        validate: input =>{
+            if(input){
+                return true
+            } else{
+                console.log(`Please type in a valid description.`)
+                return false
+            }
+        }
+    },
+    {
+        name: `liveSiteConfirm`,
+        message: `Do you want to include your live website?`,
+        type: `confirm`,
+        default: false
+    },
+    {
+        name: `liveSiteUrl`,
+        message: `Please input your live website's url:`,
+        type: `input`,
+        when: ({ liveSiteConfirm }) => {
+            if (liveSiteConfirm) {
+              return true;
+            } else {
+              return false;
+            }
+        },
+        validate: input =>{
+            if(input){
+                return true
+            } else{
+                console.log(`Please type in a valid url.`)
+                return false
+            }
+        }
+    },
+    {
+        name: `ss`,
+        message: `Do you want to use a screenshot? (Saved as assets/images/screenshot.png)`,
+        type: `confirm`,
+        default: false
+    },
+    {
+        name: `creditsConfirm`,
+        message: `Do you want to credit any collaborators?`,
+        type: `confirm`,
+        default: false
+    },
+    {
+        name: `credits`,
+        message: `Please type in the description to your credits section:`,
+        type: `input`,
+        when: ({ creditsConfirm }) => {
+            if (creditsConfirm) {
+              return true;
+            } else {
+              return false;
+            }
+        },
+        validate: input =>{
+            if(input){
+                return true
+            } else{
+                console.log(`Please type in a valid description.`)
+                return false
+            }
+        }
+    },
+    {
+        name: `licenseConfirm`,
+        message: `Do you want to include a license section?`,
+        type: `confirm`,
+        default: false
+    },
+    {
+        name:`license`,
+        message: `Which license do you wish to choose?`,
+        type: `list`,
+        choices: [
+            `Apache 2.0 License`,
+            `Boost Software License 1.0`,
+            `BSD 3-Clause License`,
+            `BSD 2-Clause License`
+        ],
+        default: `Apache 2.0 License`,
+        when: ({ licenseConfirm }) => {
+            if (licenseConfirm) {
+              return true;
+            } else {
+              return false;
+            }
+        }
+    },
+    {
+        name: `featuresConfirm`,
+        message: `Do you want to include a feature's section?`,
+        type: `confirm`,
+        default: false
+    },
+    {
+        name: `features`,
+        message: `Type in your features:`,
+        type: `input`,
+        when: ({ featuresConfirm }) => {
+            if (featuresConfirm) {
+              return true;
+            } else {
+              return false;
+            }
+        },
+        validate: input =>{
+            if(input){
+                return true
+            } else{
+                console.log(`Please type in a valid description.`)
+                return false
+            }
+        }
+    },
+    {
+        name: `contribution`,
+        message: `Would you like your application to be available to be contributed by other developers?`,
+        type: `confirm`,
+        default: false
+    },
+    {
+        name: `username`,
+        message: `What is your Github username?`,
+        type: `input`,
+        validate: input =>{
+            if(input){
+                return true
+            } else{
+                console.log(`Please type in a valid username.`)
+                return false
+            }
+        }
+    },
+    {
+        name: `emailConfirm`,
+        message: `Would you like to provide an e-mail for people to contact you?`,
+        type: `confirm`,
+        default: false
+    },
+    {
+        name: `email`,
+        message: `What is your e-mail?`,
+        type: `input`,
+        when: ({ emailConfirm }) => {
+            if (emailConfirm) {
+              return true;
+            } else {
+              return false;
+            }
+        },
+        validate: input =>{
+            if(input){
+                return true
+            } else{
+                console.log(`Please type in a valid e-mail.`)
+                return false
+            }
+        }
+
+    },
+    {
+        name: `testsConfirm`,
+        message: `Would you like to include a tests section?`,
+        type: `confirm`,
+        default: false
+    },
+    {
+        name: `tests`,
+        message: `Input a description for your tests section:`,
+        type: `input`,
+        when: ({ testsConfirm }) => {
+            if (testsConfirm) {
+              return true;
+            } else {
+              return false;
+            }
+        },
+        validate: input =>{
+            if(input){
+                return true
+            } else{
+                console.log(`Please type in a valid description.`)
+                return false
+            }
+        }
+    }
+
+])
+
+//ReadMe/Program creation 
+.then(readmeObject=>{
+    if(readmeObject.contribution){
+        writeFile(`code_of_conduct.md`, codeConduct)
+    }
+    return generateMarkdown(readmeObject)
+})
+.then(readmeText=>{
+    return writeFile(`./README.md`,readmeText)
+})
+.then(response => {
+    console.log(response)
+})
+.catch(err => {
+    console.log(err)
+})
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,176 @@
+const fs = require('fs')
+
+// Creates Readme Text
+function generateMarkdown(answerObject){
+  let {
+    name, 
+    description, 
+    toc, 
+    installation,
+    usage, 
+    liveSiteUrl, 
+    ss, 
+    credits, 
+    license, 
+    features,
+    contribution,
+    username,
+    email,
+    tests
+  } = answerObject
+
+  // Table of Contents
+  if(toc){
+    toc = `## Table of Contents\n* [Installation](#installation)\n* [Usage](#usage)\n`
+    if(credits){
+      toc += `* [Credits](#credits)\n`
+    }
+    if(license){
+      toc += `* [License](#license)\n`
+    }
+    if(features){
+      toc += `* [Features](#features)\n`
+    }
+    if(contribution){
+      toc += `* [Contribution](#contribution)\n`
+    }
+    toc += `* [Questions](#questions)\n`
+    if(tests){
+      toc += `* [Tests](#tests)`
+    }
+  } else{
+    toc = ``
+  }
+
+  // Live Site
+  if(!liveSiteUrl){
+    liveSiteUrl = ``
+  } else {
+    liveSiteUrl = `[Click to visit Live Website](${liveSiteUrl})`
+  }
+
+  // Screenshot
+  if(!ss){
+    ss = ``
+  } else {
+    ss = `\n![alt text](assets/images/screenshot.png)`
+  }
+
+  // Credits
+  if(!credits){
+    credits = ``
+  } else {
+    credits = `
+## Credits
+${credits}`
+  }
+
+  // License
+  if(!license){
+    license = ``
+    licenseBadge = ``
+  } else {
+    switch(license){
+      case `Apache 2.0 License`:
+        license = `## License\n`
+        license += `This application is covered under the Apache 2.0 License.`
+        licenseBadge = `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+        break;
+      case `Boost Software License 1.0`:
+        license = `## License\n`
+        license += `This application is covered under the Boost Software 1.0 License.`
+        licenseBadge = `[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`
+        break;
+      case `BSD 3-Clause License`:
+        license = `## License\n`
+        license += `This application is covered under the BSD 3-Clause License.`
+        licenseBadge = `[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`
+        break;
+      case `BSD 2-Clause License`:
+        license = `## License\n`
+        license += `This application is covered under the BSD 2-Clause License.`
+        licenseBadge = `[![License](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)`
+        break;
+    }
+  }
+
+  // Features
+  if(!features){
+    features = ``
+  } else {
+    features = `
+## Features  
+${features}`
+  }
+
+  // Contribution
+  if(!contribution){
+    contribution = ``
+  } else {
+    contribution = `
+## Contribution
+[![Contributor Covenant](https://img.shields.io/badge/Contributor%20Covenant-v2.0%20adopted-ff69b4.svg)](code_of_conduct.md)`
+  }
+
+  // Questions
+  username = `Click [here](https://github.com/${username}) to check out my Github!\n`
+  if(!email){
+    email = ``
+  } else {
+    email = `If you would like to contact me, please e-mail me at ${email}`
+  }
+
+  // Tests
+  if(tests){
+    tests = `
+## Tests
+${tests}`
+  } else{
+    tests = ``
+  }
+
+  // README.md
+  return `# ${name}  
+${licenseBadge}
+## Description
+${description}
+${toc}
+## Installation
+${installation}  
+${liveSiteUrl}
+## Usage
+${usage}
+${ss}
+${credits}
+${license}
+${features}
+${contribution}
+## Questions  
+${username}
+${email}
+${tests}
+`
+}
+
+function writeFile(destination, text){
+  return new Promise((resolve, reject)=>{
+    fs.writeFile(destination, text, err =>{
+      if(err){
+        reject(err)
+        return
+      }
+      resolve({
+        ok: true,
+        message: `File Created!`
+      })
+    })
+  })
+}
+
+
+
+
+
+
+
+module.exports = { generateMarkdown, writeFile }
